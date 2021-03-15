d8c57929d93780e1d6c79264dc754885bbddf7f9
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-03-15 11:11:19 +0000

    Update GBDAlcxAge.R

--- GBDAlcxAge.R
+++ GBDAlcxAge.R
@@ -6,6 +6,8 @@ library(extrafont)
 library(ggtext)
 library(ragg)
 
+#Data downloaded from http://ghdx.healthdata.org/gbd-results-tool
+
 data <- read.csv("Data/GBDAlcoholxAge.csv") %>% 
   mutate(age=factor(age, levels=c("<1 year", "1 to 4", "5 to 9", "10 to 14", "15 to 19", "20 to 24",
                                   "25 to 29", "30 to 34", "35 to 39", "40 to 44", "45 to 49", "50 to 54",
